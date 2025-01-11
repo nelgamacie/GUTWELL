d8b5b5da3a133112bce7d7965e0d3ef1590ef23e
sara <sdaher3@uwo.ca>
2025-01-11 16:43:39 -0500

    merging

--- gutwell/src/App.js
+++ gutwell/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Home from './components/home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import CalendarPage from "./components/calendar/page"; // Calendar page
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/calendar" element={<CalendarPage />} />
       </Routes>
     </Router>
